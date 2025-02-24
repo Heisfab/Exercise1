ce7945bc1ad2f02513fd021e0429319fe3e73a3c
heisfab <mucyofabrice9@gmail.com>
2025-02-24 19:34:09 +0200

    Database changed to 10

--- exercise.c
+++ exercise.c
@@ -2,31 +2,26 @@
 
 #define NUM_STUDENTS 10 // Update the number of students to 10
 
-// Structure to store student information
-struct Student {
-    char name[50];
-    int marks;
-};
-
 int main() {
-    struct Student students[NUM_STUDENTS]; // Array to hold 10 students
+    char names[NUM_STUDENTS][50]; // Array to store names of 10 students
+    int marks[NUM_STUDENTS];       // Array to store marks of 10 students
 
     // Input student names and marks
     for (int i = 0; i < NUM_STUDENTS; i++) {
         printf("Enter the name of student %d: ", i + 1);
-        fgets(students[i].name, sizeof(students[i].name), stdin);
+        fgets(names[i], sizeof(names[i]), stdin);
         // Remove newline character if present
-        students[i].name[strcspn(students[i].name, "\n")] = 0;
+        names[i][strcspn(names[i], "\n")] = 0;
 
         printf("Enter the marks of student %d: ", i + 1);
-        scanf("%d", &students[i].marks);
+        scanf("%d", &marks[i]);
         getchar(); // to consume the newline character left by scanf
     }
 
     // Output student names and marks
     printf("\n--- Student Information ---\n");
     for (int i = 0; i < NUM_STUDENTS; i++) {
-        printf("Student %d: %s, Marks: %d\n", i + 1, students[i].name, students[i].marks);
+        printf("Student %d: %s, Marks: %d\n", i + 1, names[i], marks[i]);
     }
 
     return 0;
