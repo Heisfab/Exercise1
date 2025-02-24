d32a7c1a52c5759bbe04ae9481776682b30c0aeb
heisfab <mucyofabrice9@gmail.com>
2025-02-24 19:09:01 +0200

    first commit

---
+++ exercise.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define NUM_STUDENTS 5
+
+// Structure to store student information
+struct Student {
+    char name[50];
+    int marks;
+};
+
+int main() {
+    struct Student students[NUM_STUDENTS]; // Array to hold 5 students
+
+    // Input student names and marks
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        printf("Enter the name of student %d: ", i + 1);
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        // Remove newline character if present
+        students[i].name[strcspn(students[i].name, "\n")] = 0;
+
+        printf("Enter the marks of student %d: ", i + 1);
+        scanf("%d", &students[i].marks);
+        getchar(); // to consume the newline character left by scanf
+    }
+
+    // Output student names and marks
+    printf("\n--- Student Information ---\n");
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        printf("Student %d: %s, Marks: %d\n", i + 1, students[i].name, students[i].marks);
+    }
+
+    return 0;
+}
